d168f313d2782e4a3e165482bafd80fe596c2414
Matthew Gamble <git@matthewgamble.net>
2021-03-10 19:02:25 +1100

    Use invoke in github workflow

--- tasks.py
+++ tasks.py
@@ -2,11 +2,6 @@ from invoke import task
 
 
 @task
-def test(c):
-    c.run("pytest")
-
-
-@task
 def reformat(c):
     c.run("black music_metadata_filter tests setup.py tasks.py")
 
@@ -15,6 +10,11 @@ def reformat(c):
 def lint(c):
     c.run("flake8 music_metadata_filter tests tasks.py")
     c.run("black --check --diff music_metadata_filter tests setup.py")
+
+
+@task
+def test(c):
+    c.run("pytest")
 
 
 @task
